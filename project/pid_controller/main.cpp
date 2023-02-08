d47b5d9db3b184744327f483bcbdb031153545e5
Chan Chun Fong <chun.fong.chan.j4d@jp.denso.com>
2023-02-08 10:30:13 +0900

    first commit

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -195,6 +195,15 @@ void set_obst(vector<double> x_points, vector<double> y_points, vector<State>& o
 	obst_flag = true;
 }
 
+double angle_correction(double angle){
+  while(abs(angle) > M_PI){
+    if(angle < -M_PI) angle += 2 * M_PI;
+    if(angle > M_PI) angle -= 2 * M_PI; 
+  }
+  return angle;
+}
+
+
 int main ()
 {
   cout << "starting server" << endl;
@@ -218,15 +227,19 @@ int main ()
   /**
   * TODO (Step 1): create pid (pid_steer) for steer command and initialize values
   **/
-
+  PID pid_steer = PID();
+  double max_steer = 1.2;    //original value, may vary based on result
+  pid_steer.Init(0.25, 0.1, 0.5, max_steer , -max_steer, 10);    //original value setup
 
   // initialize pid throttle
   /**
   * TODO (Step 1): create pid (pid_throttle) for throttle command and initialize values
   **/
-
-  PID pid_steer = PID();
   PID pid_throttle = PID();
+  double max_throttle = 1;
+  double max_break = -1;
+  pid_throttle.Init(0.25, 0.05, 0.1, max_throttle, max_break, 10);      //original value setup;
+
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
@@ -289,34 +302,45 @@ int main ()
           * TODO (step 3): uncomment these lines
           **/
 //           // Update the delta time with the previous command
-//           pid_steer.UpdateDeltaTime(new_delta_time);
+          pid_steer.UpdateDeltaTime(new_delta_time);
 
           // Compute steer error
           double error_steer;
 
 
           double steer_output;
+          double dis_min = 10000;
+          int close_id = 0;
 
           /**
           * TODO (step 3): compute the steer error (error_steer) from the position and the desired trajectory
           **/
-//           error_steer = 0;
+          for (int i = 0; i < x_points.size(); ++i)
+          {
+            double action_distance = pow((x_position - x_points[i]), 2) + pow((y_position - y_points[i]), 2);
+            if(action_distance < dis_min){
+              dis_min = action_distance;
+              close_id = i;
+            }
+          }
+          error_steer = angle_between_points(x_position, y_position, x_points[close_id], y_points[close_id]) - yaw;
 
           /**
           * TODO (step 3): uncomment these lines
           **/
 //           // Compute control to apply
-//           pid_steer.UpdateError(error_steer);
-//           steer_output = pid_steer.TotalError();
+          pid_steer.UpdateError(error_steer);
+          steer_output = pid_steer.TotalError();
 
 //           // Save data
-//           file_steer.seekg(std::ios::beg);
-//           for(int j=0; j < i - 1; ++j) {
-//               file_steer.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-//           }
-//           file_steer  << i ;
-//           file_steer  << " " << error_steer;
-//           file_steer  << " " << steer_output << endl;
+          file_steer.seekg(std::ios::beg);
+          for(int j=0; j < i - 1; ++j)
+          {
+              file_steer.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+          }
+          file_steer  << i ;
+          file_steer  << " " << error_steer;
+          file_steer  << " " << steer_output << endl;
 
           ////////////////////////////////////////
           // Throttle control
@@ -326,7 +350,7 @@ int main ()
           * TODO (step 2): uncomment these lines
           **/
 //           // Update the delta time with the previous command
-//           pid_throttle.UpdateDeltaTime(new_delta_time);
+          pid_throttle.UpdateDeltaTime(new_delta_time);
 
           // Compute error of speed
           double error_throttle;
@@ -335,8 +359,7 @@ int main ()
           **/
           // modify the following line for step 2
           error_throttle = 0;
-
-
+          error_throttle = v_points[close_id] - velocity;
 
           double throttle_output;
           double brake_output;
@@ -345,27 +368,30 @@ int main ()
           * TODO (step 2): uncomment these lines
           **/
 //           // Compute control to apply
-//           pid_throttle.UpdateError(error_throttle);
-//           double throttle = pid_throttle.TotalError();
+          pid_throttle.UpdateError(error_throttle);
+          double throttle = pid_throttle.TotalError();
 
 //           // Adapt the negative throttle to break
-//           if (throttle > 0.0) {
-//             throttle_output = throttle;
-//             brake_output = 0;
-//           } else {
-//             throttle_output = 0;
-//             brake_output = -throttle;
-//           }
+          if (throttle > 0.0) 
+          {
+            throttle_output = throttle;
+            brake_output = 0;
+          } 
+          else 
+          {
+            throttle_output = 0;
+            brake_output = -throttle;
+          }
 
 //           // Save data
-//           file_throttle.seekg(std::ios::beg);
-//           for(int j=0; j < i - 1; ++j){
-//               file_throttle.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-//           }
-//           file_throttle  << i ;
-//           file_throttle  << " " << error_throttle;
-//           file_throttle  << " " << brake_output;
-//           file_throttle  << " " << throttle_output << endl;
+          file_throttle.seekg(std::ios::beg);
+          for(int j=0; j < i - 1; ++j){
+              file_throttle.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+          }
+          file_throttle  << i ;
+          file_throttle  << " " << error_throttle;
+          file_throttle  << " " << brake_output;
+          file_throttle  << " " << throttle_output << endl;
 
 
           // Send control
--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -19,6 +19,13 @@ void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, doubl
    /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    **/
+   this->Kpi = Kpi;
+   this->Kii = Kii;
+   this->Kdi = Kdi;
+   this->output_lim_maxi = output_lim_maxi;
+   this->output_lim_mini = output_lim_mini;
+   cte0 = 0;
+   I = 0;
 }
 
 
@@ -26,6 +33,12 @@ void PID::UpdateError(double cte) {
    /**
    * TODO: Update PID errors based on cte.
    **/
+   if(abs(delta_time) < 0.000001) return;
+   double P = Kpi * cte;
+   I += Kii * cte * delta_time;
+   double D = Kdi * (cte - cte0) / delta_time;
+   action = P + I + D;
+   cte0 = cte;
 }
 
 double PID::TotalError() {
@@ -33,12 +46,16 @@ double PID::TotalError() {
    * TODO: Calculate and return the total error
     * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
    */
-    double control;
-    return control;
+   double control = action;
+   if (action < output_lim_mini) control = output_lim_mini;
+   if (action > output_lim_maxi) control = output_lim_maxi;
+   return control;
 }
 
 double PID::UpdateDeltaTime(double new_delta_time) {
    /**
    * TODO: Update the delta time with new value
    */
+   delta_time = new_delta_time;
+   return delta_time;
 }
